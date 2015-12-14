dac4669ec3c60edba50e81bfcc280c67b00eeb10
Harry van der Valk <xxlfalcon@gmail.com>
2015-12-14 17:47:36 +0100

    Foo class

---
+++ src/frissr/volunteer/Entity/Foo.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Harry van der Valk - The Hague Intelliguence Group
+ * Date: 14-12-15
+ * Time: 17:47
+ */
+
+namespace Frissr\Volenteer\Entity;
+
+
+class Foo {
+
+} 
