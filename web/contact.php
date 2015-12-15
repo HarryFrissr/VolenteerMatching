0f37bd199ffba280be43fd667c25f241f7005c20
Hiltje Conradi <Hiltje@MacBook-Pro-van-Hiltje.local>
2015-12-15 13:53:23 +0100

    changes made

--- web/contact.php
+++ web/contact.php
@@ -5,6 +5,8 @@
  * Date: 12-12-15
  * Time: 11:25
  */
+error_reporting(E_ALL);
+ini_set("display_errors", "1");
 
 require_once __DIR__ . './../vendor' . '/autoload.php';
 
--- web/index.php
+++ web/index.php
@@ -1,4 +1,5 @@
 <?php
+
 // TODO Maak hier de FrontController. De Frontcontroller heeft de volgende taken:
 // TODO - Eenmalig de autoload aanroepen
 // TODO - Alle verzoeken van de website gaan via de FrontController
@@ -7,7 +8,6 @@
 // TODO - Maak aparte Views aan en laad deze vanuit de app/views folder
 // TODO - Geef de parameters door aan de Views
 // TODO - Maak een default layout aan
-
 require_once __DIR__ . './../vendor' . '/autoload.php';
 
 echo "Welkom op de Matching website voor Vluchtelingen en Vrijwilligers in Nederland";
