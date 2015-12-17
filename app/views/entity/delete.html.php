62f9609ba32b7b6553ebc6eb73538ec4abec3daf
Harry van der Valk <xxlfalcon@gmail.com>
2015-12-17 18:18:03 +0100

    Commit the changes. Sample CRUD

---
+++ app/views/entity/delete.html.php
@@ -0,0 +1,3 @@
+<h2>Delete Object</h2>
+
+Do you want to delete this object
---
+++ web/entity_update.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Harry van der Valk - The Hague Intelliguence Group
+ * Date: 17-12-15
+ * Time: 17:47
+ */
+
+require_once __DIR__ . './../vendor' . '/autoload.php';
+
+use Frissr\Volunteer\Controller\EntityController;
+
+$controller = new EntityController();
+echo $controller->updateAction();
