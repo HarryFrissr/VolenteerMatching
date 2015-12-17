3d38603b3de6575a901e2c5d26b3c9f7a3b1363f
Harry van der Valk <xxlfalcon@gmail.com>
2015-12-17 11:03:54 +0100

    Docs Extra classes Service by container

---
+++ web/services.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Harry van der Valk - The Hague Intelliguence Group
+ * Date: 17-12-15
+ * Time: 10:52
+ */
+
+// TODO A second require_once. Try to use a Front Controller instead.
+require_once __DIR__ . './../vendor' . '/autoload.php';
+require_once __DIR__ . './../app/config' . '/services.php';
+
+?>
+
+<h2>Get services</h2>
+
+<?php
+
+$foo = $container->get('foo');
+
+echo $foo;
+
