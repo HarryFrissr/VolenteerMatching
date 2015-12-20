3db1e35e693f586db17877139092025de09499b4
JMitnik <JMitnik@gmail.com>
2015-12-20 17:35:32 +0100

    Attempt 1 at databases

--- app/config/services.php
+++ app/config/services.php
@@ -12,7 +12,6 @@ use Faker\Factory;
 use Symfony\Component\Templating\PhpEngine;
 use Symfony\Component\Templating\TemplateNameParser;
 use Symfony\Component\Templating\Loader\FilesystemLoader;
-
 /**
  * A container is just the place to put object instances. You can call the instances - services - from the container by
  * name.
@@ -22,9 +21,30 @@ use Symfony\Component\Templating\Loader\FilesystemLoader;
  *   echo $foo;
  */
 
+use Doctrine\Common\ClassLoader;
+
+require '../../vendor/doctrine/common/lib/Doctrine/Common/ClassLoader.php';
+
+$classLoader = new ClassLoader('Doctrine', '/path/to/doctrine');
+$classLoader->register();
+
+$config = new \Doctrine\DBAL\Configuration();
+$connectionParams = array(
+    'dbname' => 'database',
+    'user' => 'user',
+    'password' => 'password',
+    'host' => 'localhost',
+    'port' => 3306,
+    'charset' => 'utf8',
+    'driver' => 'pdo_mysql',
+);
+$db = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
+
 $container = new ContainerBuilder();
 
 $loader = new FilesystemLoader(__DIR__.'/../views/%name%');
+
+
 $session = new Symfony\Component\HttpFoundation\Session\Session();
 $session->start();
 
@@ -35,8 +55,26 @@ $faker = Factory::create();
 $container->set('templating', $templating);
 
 // Zet een service - bij instantie
+
 $container->set('faker', $faker);
 $container->set('session' , $session);
+$container->set('db',$db);
+
+$schema = new Doctrine\DBAL\Schema\Schema();
+$usersTable = $schema->createTable("Users");
+
+$usersTable->addColumn("id", "integer", array("unsigned" => true));
+$usersTable->addColumn("first_name", "string", array("length" => 64));
+$usersTable->addColumn("last_name", "string", array("length" => 64));
+$usersTable->addColumn("email", "string", array("length" => 256));
+$usersTable->addColumn("website", "string", array("length" => 256));
+
+$usersTable->setPrimaryKey(array("id"));
+
+$platform = $conn->getDatabasePlatform();
+$queries = $schema->toSql($platform);
+
+
 
 // Registeer een service - bij naam
 $container->register('foo', 'Frissr\Volunteer\Entity\Foo');
