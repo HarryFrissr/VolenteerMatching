1db5eb1336f84a30eded45c2b46e7de238081db4
Harry van der Valk <xxlfalcon@gmail.com>
2015-12-21 12:24:29 +0100

    Added the frontController

--- src/frissr/FrontController/FrontController.php
+++ src/frissr/FrontController/FrontController.php
@@ -2,8 +2,17 @@
 
 namespace Frissr\FrontController;
 
+use ReflectionClass;
+use InvalidArgumentException;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
 class FrontController implements FrontControllerInterface
 {
+    /**
+     * @var ContainerBuilder
+     */
+    protected $container;
+
     const DEFAULT_CONTROLLER = "IndexController";
     const DEFAULT_ACTION     = "index";
     
@@ -12,32 +21,20 @@ class FrontController implements FrontControllerInterface
     protected $params        = array();
     protected $basePath      = "src/";
     
-    public function __construct(array $options = array()) {
-        if (empty($options)) {
-           $this->parseUri();
-        }
-        else {
-            if (isset($options["controller"])) {
-                $this->setController($options["controller"]);
-            }
-            if (isset($options["action"])) {
-                $this->setAction($options["action"]);     
-            }
-            if (isset($options["params"])) {
-                $this->setParams($options["params"]);
-            }
-        }
+    public function __construct(ContainerBuilder $container) {
+        $this->container = $container;
+        $this->parseUri();
     }
     
     protected function parseUri() {
-        $path = trim(parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH), "/");
-        $path = preg_replace('/[^a-zA-Z0-9]/', "", $path);
-        if (strpos($path, $this->basePath) === 0) {
-            $path = substr($path, strlen($this->basePath));
-        }
-        @list($controller, $action, $params) = explode("/", $path, 3);
-        var_dump($controller);
-        var_dump($action);
+        reset($_GET);
+        $stack = explode('/', key($_GET));
+        $controller = array_shift($stack);
+        $controller = (isset($controller) ? $controller : 'index') . 'Controller';
+        $action = array_shift($stack);
+        $action = (isset($action) ? $action : 'index') . 'Action' ;
+        $params = $stack;
+
         if (isset($controller)) {
             $this->setController($controller);
         }
@@ -45,12 +42,13 @@ class FrontController implements FrontControllerInterface
             $this->setAction($action);
         }
         if (isset($params)) {
-            $this->setParams(explode("/", $params));
+            $this->setParams($stack);
         }
     }
     
     public function setController($controller) {
-        $controller = ucfirst(strtolower($controller)) . "Controller";
+        // TODO Remove hard-code namespace
+        $controller = 'Frissr\\Volunteer\\Controller\\' . ucfirst(strtolower($controller));
         if (!class_exists($controller)) {
             throw new InvalidArgumentException(
                 "The action controller '$controller' has not been defined.");
@@ -75,6 +73,6 @@ class FrontController implements FrontControllerInterface
     }
     
     public function run() {
-        call_user_func_array(array(new $this->controller, $this->action), $this->params);
+        return call_user_func_array(array(new $this->controller(), $this->action), $this->params);
     }
 }
---
+++ src/frissr/volunteer/Controller/PersonController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Harry van der Valk - The Hague Intelligence Group
+ * Date: 21-12-15
+ * Time: 12:05
+ */
+
+namespace Frissr\Volunteer\Controller;
+
+
+class PersonController {
+
+    public function indexAction() {
+        // TODO Do nothing
+        return 'I am a Controller. Called by the front Controller';
+    }
+} 
