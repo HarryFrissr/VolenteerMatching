8bbe5847e4b8f6c3eda30e705f392a5be8ca6baa
mpisters <m.pisters@student.vu.nl>
2015-12-21 13:58:44 +0100

    database sql

--- app/config/services.php
+++ app/config/services.php
@@ -45,18 +45,71 @@ $container->set('fixed_refugee_list', new Frissr\Volunteer\Service\FixedRefugeeS
 $container->register('message_service', 'Frissr\Volunteer\Service\MessageService');
 $container->register('send_message_service', 'Frissr\Volunteer\Service\SendMessageService');
 
+
+// DBAL service
+
+use Doctrine\Common\ClassLoader;
+require '../vendor/doctrine/common/lib/Doctrine/Common';
+$classloader = new ClassLoader('Doctrine', './Doctrine/doctrine-dbal');
+$classloader->register();
+
 $config = new \Doctrine\DBAL\Configuration();
 //..
 $connectionParams = array(
-    // When using mySQL
-//    'dbname' => 'mydb',
-//    'user' => 'user',
-//    'password' => 'secret',
-//    'host' => 'localhost',
-//    'driver' => 'pdo_mysql',
-
-    // When using sqlite
-    'url' => 'sqlite://db.sqlite'
+    'dbname' => 'db_michelle',
+    'user' => 'user',
+    'password' => 'secret',
+    'host' => 'localhost',
+    'driver' => 'pdo_mysql',
 );
+
+
 $conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
 $container->set('db', $conn);
+
+$schema = new \Doctrine\DBAL\Schema\Schema();
+//Person Table
+$personTable = $schema -> createTable("Person");
+$personTable -> addColumn("id", "integer", array("unsigned" => true));
+$personTable -> addColumn("first_name", "string", array("length" => 64));
+$personTable -> addColumn("last_name", "string", array("length" => 64));
+$personTable -> addColumn("e-mail", "string", array("length" => 256));
+
+$personTable -> addColumn("profile_image", "object", array("length" => 1));
+$personTable ->addColumn("birthday", "string", array("length"=>8));
+
+//Interest Table
+$interestTable = $schema ->createTable("Interests");
+$interestTable ->addColumn("name", "string", array("length" => 256));
+$interestTable ->addColumn("category", "string", array("length" => 256));
+
+// Event Table
+$eventTable = $schema -> createTable("Events");
+$eventTable ->addColumn("title", "string", array("length" => 256));
+$eventTable ->addColumn("description", "text", array("length" => 256));
+$eventTable ->addColumn("date", "date", array("length" => 256));
+$eventTable ->addColumn("time","time", array("length" => 256));
+$eventTable ->addColumn("maxNumberOfParticipants", "integer", array("unsigned" => true));
+$eventTable ->addColumn("NumberOfParticipants", "integer", array("unsigned" => true));
+$eventTable ->addColumn("location", "string", array("length" => 256));
+$eventTable ->addColumn("organiser", "string", array("length" => 256));
+
+//Foo Table
+$fooTable = $schema -> createTable("Foo");
+$fooTable ->addColumn("title", "string", array("length" => 256));
+
+//Location Table
+$locationTable = $schema -> createTable("Location");
+$locationTable ->addColumn("City", "string", array("length" => 256));
+$locationTable ->addColumn("street", "string", array("length" => 256));
+$locationTable ->addColumn("streetNumber", "integer", array("unsigned" => true));
+$locationTable ->addColumn("zip code", "string", array("length" => 10));
+
+//Refugee/Volunteer Table
+$refugee_volunteerTable = $schema -> createTable("refugee&volunteerTable");
+$refugee_volunteerTable -> addColumn("refugee", "boolean");
+$refugee_volunteerTable -> addColumn("volunteer", "boolean");
+
+$platform = $conn -> getDatabasePlatform();
+$queries = $schema -> toSql($platform);
+
