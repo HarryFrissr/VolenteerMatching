3640558c76729dedc439a04b2b3d57e3316d56d5
Hiltje Conradi <Hiltje@MacBook-Pro-van-Hiltje.local>
2015-12-21 13:14:07 +0100

    Translation work in progress

---
+++ src/Frissr/Volunteer/Service/TranslationService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hiltje
+ * Date: 20-12-15
+ * Time: 17:17
+ */
+
+namespace Frissr\Volunteer\Service;
+
+
+class TranslationService
+{
+
+}
---
+++ src/Frissr/Volunteer/Service/db.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hiltje
+ * Date: 21-12-15
+ * Time: 13:05
+ */
+
+namespace Frissr\Volunteer\Service;
+
+
+class db
+{
+
+}
