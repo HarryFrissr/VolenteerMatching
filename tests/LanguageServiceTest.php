9e95407912dfcb67ed0962dc7277396066f36e3b
Hiltje Conradi <Hiltje@MacBook-Pro-van-Hiltje.local>
2015-12-22 11:39:48 +0100

    translation unit testing, work in progress

---
+++ tests/LanguageServiceTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Hiltje
+ * Date: 21-12-15
+ * Time: 17:21
+ */
+class LanguageServiceTest extends PHPUnit_Framework_TestCase
+{
+
+}
---
+++ web/db_list_events.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hiltje
+ * Date: 21-12-15
+ * Time: 18:51
+ */
