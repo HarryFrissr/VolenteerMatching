2e1027d841ddf739fe4966c80afe126d6c839e4b
mpisters <m.pisters@student.vu.nl>
2015-12-23 19:05:15 +0100

    migratetable EVENT

---
+++ src/Frissr/Volunteer/Command/Migration/migrateTableEvent.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Michelle
+ * Date: 23-12-2015
+ * Time: 18:58
+ */
+
+namespace Frissr\Volunteer\Command\Migration;
+
+
+class migrateTableEvent
+{
+
+}
