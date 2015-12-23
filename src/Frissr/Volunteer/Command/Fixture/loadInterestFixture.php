aa8c4538eb8dec4c36132a82b3de3b6e0f4bea43
JMitnik <JMitnik@gmail.com>
2015-12-23 20:00:23 +0100

    Typo fixture frisr_demo

---
+++ src/Frissr/Volunteer/Command/Fixture/loadInterestFixture.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jonathan
+ * Date: 12/23/2015
+ * Time: 7:53 PM
+ */
+
+namespace Frissr\Volunteer\Command\Fixture;
+
+
+class loadInterestFixture
+{
+
+}
---
+++ src/Frissr/Volunteer/Command/Migration/migrateTable122320151845.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jonathan
+ * Date: 12/23/2015
+ * Time: 6:45 PM
+ */
+
+namespace Frissr\Volunteer\Command\Migration;
+
+
+class migrateTable122320151845
+{
+
+}
