119a11baa0a6004bde63957345bb447dec46c954
mpisters <m.pisters@student.vu.nl>
2016-01-03 22:47:02 +0100

    - fixed the misspelling - removed the hard coded source in frontcontroller - changed the sql messages to one sql message

--- src/frissr/volunteer/Command/Migration/migrateTableMessages.php
+++ src/frissr/volunteer/Command/Migration/migrateTableMessages.php
@@ -16,20 +16,14 @@ class migrateTableMessages extends BaseMigrationTable {
         return 'Create Match Table';
     }
 
-    // TODO Hier worden drie SQL statements in één methode terug gegeven. Kijk of je het in een array kan terug geven?
+
     protected function getSQL() {
         return '  CREATE TABLE `messages` (
-                  `id` int(11) NOT NULL,
+                  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
                   `sender` int(11) NOT NULL,
                   `receiver` int(11) NOT NULL,
                   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                   `content` text NOT NULL
-                );
-                ALTER TABLE `messages`
-                  ADD PRIMARY KEY (`id`);
-
-                ALTER TABLE `messages`
-                  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-                ';
+              );';
     }
 }
